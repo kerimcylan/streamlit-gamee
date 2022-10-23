da68d5e3058fbaeec2c2f2650b067c894f5f6ced
kerimcylan <71334905+kerimcylan@users.noreply.github.com>
2022-10-23 22:22:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ df = df[df["activation_Year_"] == year_filter]
 placeholder = st.empty()
 for seconds in range(200):
 #while True: 
-    
+    df['activation_Year_new'] = df['activation_Year_'] * np.random.choice(range(1,5))
     df['activation_Genre_new'] = df['activation_Genre_'] * np.random.choice(range(1,5))
     df['activation_Platform_new'] = df['activation_Platform_'] * np.random.choice(range(1,5))
 
@@ -65,11 +65,11 @@ for seconds in range(200):
         with fig_col1:
             st.markdown("### First Chart")
             fig = px.density_heatmap(
-            data_frame=df, y="count", x="activation_Year_")
+            data_frame=df, y="count", x="activation_Year_new")
             st.write(fig)
         with fig_col2:
             st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame=df, x="activation_Genre_")
+            fig2 = px.histogram(data_frame=df, x="activation_Genre_new")
             st.write(fig2)
         st.markdown("### Detailed Data View")
         st.dataframe(df)
