fd1b392d1124c9ae4462d90c89e0debaf33a5b0b
kerimcylan <71334905+kerimcylan@users.noreply.github.com>
2022-10-24 01:18:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ st.set_page_config(
     layout="wide",
 )
 
-st.metric(label="Platform", value=str('activations_Platform_'), delta=-0.5,
+st.metric(label="Platform", value=str('activations_Platform_'), delta=-None,
     delta_color="inverse")
 
 
