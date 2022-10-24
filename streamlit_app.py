05539b4a991333af9e39d978899fe57099461d5d
kerimcylan <71334905+kerimcylan@users.noreply.github.com>
2022-10-24 13:11:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,12 +46,12 @@ fig_col1, fig_col2 = st.columns(2)
 
 with fig_col1:
     st.markdown("### First Chart")
-    fig = px.density_heatmap(data_frame=df, y="count", x="activation_Year_")
+    fig = px.pie(data_frame=df, value="count", x="activation_Year_")
     st.write(fig)
    
 with fig_col2:
     st.markdown("### Second Chart")
-    fig2 = px.histogram(data_frame=df, x="activation_Genre_")
+    fig2 = px.histogram(data_frame=df, x="activation_Genre_",color='count')
     st.write(fig2)
   
 
