a1aee01c61af9ccea8d024a1d326bef406e9fadc
kerimcylan <71334905+kerimcylan@users.noreply.github.com>
2022-10-24 16:18:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,36 +27,5 @@ sql = """
 
 
 df = client.query(sql).to_dataframe()
-df_utils=df.dropna(axis=0)
-df_utils.reset_index(drop=True,inplace=True)
-a=df_utils['activation_Year_'].str.split(" ")
-# for publish date
-b=[]
-for i in range(0,len(a)):
- c=a[i][0]
- b.append(c)
-#for status date
-d=df_utils['activation_Genre_'].str.split(" ")
-e=[]
-for i in range(0,len(a)):
- f=d[i][0]
- e.append(f)
-df_utils['activation_Publisher_']=b
-df_utils['activation_Year_']=e
-g=df_utils['count'].str.split('/')
-h=[]
-for i in range(0,len(a)):
- f=g[i][2]
- h.append(f)
-df_utils['activation_Genre_']=h
 
-st.header("Real-Time Dashboard")
-chart_selector = st.sidebar.selectbox("Select the type of chart", ['Pie chart','Bar chart'])
-if chart_selector=='Pie chart':
-  st.write("## Pie chart analysis for various crime")
-  pie_chart = px.pie(df_utils, values='activation_Year_', names='activation_Publisher_')
-  st.plotly_chart(pie_chart,use_container_width = True)
-else:
-  st.write("## Bar chart analysis for crime varying according time")
-  bar_chart = px.histogram(df_utils, x="count", color="activation_Genre")
-  st.plotly_chart(bar_chart,use_container_width = True)
+st.title("Game Count Dashboard")
